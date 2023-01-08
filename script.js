cf4ad963b2f6af5a31d3ce387548a8c6ed8827e1
DarkCoder2256 <122190616+DarkCoder2256@users.noreply.github.com>
2023-01-08 09:51:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const Btn = document.querySelector(".card-body")
+const learnMore = document.querySelector(".learnmore")
+
+function show(){
+ 
+    Btn.style.display = "block"
+    learnMore.style.display = "none"
+
+}
+
